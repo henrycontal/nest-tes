f48fb1caa1faf45edf734dc1043d250ae4edbe33
Enrique Contreras <brayancontreras@solemti.mx>
2023-04-12 17:28:56 -0700

    test: env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return `Hello World!, current env -> ${process.env.NODE_ENV}`;
+        return `${JSON.stringify(process.env, null, 4)}`;
     }
 }
