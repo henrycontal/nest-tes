7fc9a84cdf1621cd24d4ee027950358b962c5ba5
Enrique Contreras <brayancontreras@solemti.mx>
2023-04-17 13:02:45 -0700

    rollback v1.1

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { AwsSmmProviderModule } from './aws/ssm/provider.module';
 import { MongoDatabaseProviderModule } from './database/mongo/provider.module';
 
 @Module({
-    imports: [AwsSmmProviderModule, MongoDatabaseProviderModule],
-    exports: [AwsSmmProviderModule, MongoDatabaseProviderModule],
+    imports: [MongoDatabaseProviderModule],
+    exports: [MongoDatabaseProviderModule],
 })
 export class ProvidersModule {}
