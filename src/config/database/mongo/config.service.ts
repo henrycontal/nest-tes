f4aa4c650b711597f6b1d3b907321824d97273c7
Enrique Contreras <brayancontreras@solemti.mx>
2023-04-17 18:01:58 -0700

    ssm v6

--- src/config/database/mongo/config.service.ts
+++ src/config/database/mongo/config.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { PSConfigService } from 'nestjs-param-store';
 
 @Injectable()
 export class MongoConfigService {
-    constructor(private readonly config: ConfigService) {}
+    constructor(
+        private readonly config: ConfigService,
+        private readonly ps: PSConfigService,
+    ) {}
 
     public get URI(): string {
-        return this.config.get<string>('mongo.uri');
+        const host = this.ps.get('MONGO_HOST_TEST');
+        const credentials = this.ps.get('MONGO_CREDENTIALS_TEST');
+        const database = this.ps.get('MONGO_DATABASE_TEST');
+
+        const uri = this.config.get<string>('mongo.uri');
+
+        return uri
+            .replace('{credentials}', credentials)
+            .replace('{host}', host)
+            .replace('{database}', database);
     }
 }
--- src/providers/database/mongo/provider.module.ts
+++ src/providers/database/mongo/provider.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { PSConfigService } from 'nestjs-param-store';
 import { MongoConfigModule } from '../../../config/database/mongo/config.module';
 import { MongoConfigService } from '../../../config/database/mongo/config.service';
 
@@ -8,31 +7,14 @@ import { MongoConfigService } from '../../../config/database/mongo/config.servic
     imports: [
         MongooseModule.forRootAsync({
             imports: [MongoConfigModule],
-            useFactory: async (
-                config: MongoConfigService,
-                ssm: PSConfigService,
-            ) => {
-                const host = ssm.get('MONGO_HOST_TEST');
-                const credentials = ssm.get('MONGO_CREDENTIALS_TEST');
-                const database = ssm.get('MONGO_DATABASE_TEST');
-
-                console.log({
-                    host,
-                    credentials,
-                    database,
-                });
-
-                const uri = config.URI.replace('{credentials}', credentials)
-                    .replace('{host}', host)
-                    .replace('{database}', database);
-
-                console.log(uri);
+            useFactory: async (config: MongoConfigService) => {
+                console.log(config.URI);
 
                 return {
-                    uri,
+                    uri: config.URI,
                 };
             },
-            inject: [MongoConfigService, PSConfigService],
+            inject: [MongoConfigService],
         }),
     ],
     providers: [],
